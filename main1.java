72d48551837ca1610d4c999abee47a56c047755b
ayman2ppp2 <whiteshadow2ppp2@gmail.com>
2023-01-27 17:06:20 +0200

    pmproject

---
+++ main1.java
@@ -0,0 +1,13 @@
+
+public class main1 {
+     public static void main(String[] args) {
+          new homepage();
+          // new loginpage();
+          // new startpage();
+          // new returned();
+          // new admin();
+          // new edit();
+          // new logs();
+
+     }
+}
